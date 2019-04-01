2e102906d72715282764e7d51893f0dda5ea3cb8
Benas <benas.jakusevas@gmail.com>
2019-04-01 13:46:00 +0300

    Create index_HW7.py
    Atliktas namu darbas

---
+++ index_HW7.py
@@ -0,0 +1,45 @@
+# Homework 7.1
+
+# nuotaika = input("kokia jusu nuotaika? " )
+#
+# if nuotaika == "gera":
+#     print("Malonu matyti jus geros nuotaikos")
+# elif nuotaika == "nervinga":
+#     print("ikvepk giliai tris kartus")
+# elif nuotaika == "bloga":
+#     print("suvalgyk ledu")
+# elif nuotaika == "liudna":
+#     print("paziurek komedija")
+# else:
+#     print("viskas bus gerai :)")
+
+
+#=========================================================
+# Homework 7.2
+
+# slaptas_skaicius = 9
+#
+# spejimas = int(input("iveskite spejima nuo 0 iki 9: " ))
+#
+# if spejimas == slaptas_skaicius:
+#     print("sveikinu atspejus")
+# else:
+#     print("bandykite dar karta")
+
+
+#=========================================================
+# Homework 7.3
+
+# pirmas_skaicius = int(input("iveskite pirma skaiciu: "))
+# # antras_skaicius = int(input("iveskite antra skaiciu: "))
+# # veiksmas = input("iveskite norima atlikti veiksma +, -, *, /  : ")
+# #
+# # if veiksmas =="+":
+# #     print(pirmas_skaicius+antras_skaicius)
+# # elif veiksmas =="-":
+# #     print(pirmas_skaicius-antras_skaicius)
+# # elif veiksmas =="*":
+# #     print(pirmas_skaicius*antras_skaicius)
+# # elif veiksmas =="/":
+# #     print(round(pirmas_skaicius/antras_skaicius, 2))
+# #
