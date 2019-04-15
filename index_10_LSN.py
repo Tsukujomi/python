e1af31d557e4de311018e27de0ac5f27d7ab7dc9
Benas <benas.jakusevas@gmail.com>
2019-04-15 17:18:50 +0300

    10 pamoka

--- index_10_LSN.py
+++ index_10_LSN.py
@@ -1,12 +1,12 @@
-# #pirmas būdas išrtraukti info iš failo
-# ====================================================================
+# # #pirmas būdas išrtraukti info iš failo
+# # ====================================================================
 # with open('rezultatas.txt') as failas:
 #     rezultatas = failas.read()
 #     print(rezultatas)
 #
-
-# #antras būdas išrtraukti info iš failo
-# ====================================================================
+#
+# # #antras būdas išrtraukti info iš failo
+# # ====================================================================
 # failas = open('rezultatas.txt')
 # rezultatas = failas.read()
 # print(rezultatas)
@@ -20,13 +20,38 @@
 #     print(rezultatas)
 
 
-with open('km.txt') as failas:
-    rezultatas = failas.read()
-    sarasas = rezultatas.split('/n')
+# with open('km.txt') as failas:
+#     rezultatas = failas.read()
+#     sarasas = rezultatas.split('/n')
+#
+# for i in sarasas:
+#     sk = int(sarasas)
+#     sk = str(sk*1.6)
+#     with open('mylios.txt', 'w') as failas:
+#         for x in sarasas:
+#             failas.write(sk)
 
-for i in sarasas:
-    sk = int(sarasas)
-    sk = str(sk*1.6)
-    with open('mylios.txt', 'w') as failas:
-        for x in sarasas:
-            failas.write(sk)
+import random
+
+secret = random.randint(1, 30)
+attempts = 0
+
+with open("rezultatas.txt", "r") as score_file:
+    best_score = int(score_file.read())
+    print("Top score (attempts): " + str(best_score))
+
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
+
+    if guess == secret:
+        print("You've guessed it - congratulations! It's number " + str(secret))
+        print("Attempts needed: " + str(attempts))
+        if attempts < best_score:
+            with open("rezultatas.txt", "w") as score_file:
+                score_file.write(str(attempts))
+        break
+    elif guess > secret:
+        print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
