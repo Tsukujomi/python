673d19a874828cf42e330595c312d24120d8c7c1
Benas <benas.jakusevas@gmail.com>
2019-04-16 14:13:09 +0300

    11 pamoka komentarai paaiškinimams

--- index_11.py
+++ index_11.py
@@ -60,7 +60,7 @@ with open("score_list.txt", "r") as score_file:
     print("Top scores: " + str(score_list))
 
 for score_dict in score_list:
-    print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+    print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))#paaiškinti kaip veikia
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
@@ -70,7 +70,7 @@ while True:
         print("You've guessed it - congratulations! It's number " + str(secret))
         print("Attempts needed: " + str(attempts))
 
-        score_list.append({'attempts': attempts, "date": str(datetime.datetime.now())})
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})#paaiškinti kaip veikia
 
         with open("score_list.txt", "w") as score_file:
             score_file.write(json.dumps(score_list))
