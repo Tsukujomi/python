259a3da72e71d3ce2e770a249650dd31a008f65e
Benas <benas.jakusevas@gmail.com>
2019-04-16 14:09:01 +0300

    11 pamoka - list/dictionaries

--- index_11.py
+++ index_11.py
@@ -14,15 +14,19 @@ listas = [1, 2, 3]
 # #listo ilgis - len
 # print(len(listas))
 
+# import random
+# import json
+#
+# secret = random.randint(1, 30)
+# attempts = 0
+#
+#
 # with open("score_list.txt", "r") as score_file:
 #     score_list = json.loads(score_file.read())
 #     print("Top scores: " + str(score_list))
 #
-# while True:
-# import random
-#
-# secret = random.randint(1, 30)
-# attempts = 0
+# score_list.sort()
+# print(score_list[:3])
 #
 # while True:
 #     guess = int(input("Guess the secret number (between 1 and 30): "))
@@ -31,19 +35,47 @@ listas = [1, 2, 3]
 #     if guess == secret:
 #         print("You've guessed it - congratulations! It's number " + str(secret))
 #         print("Attempts needed: " + str(attempts))
+#         score_list.append(attempts)
+#         print(score_list)
+#         with open("score_list.txt", "w") as score_file:
+#             score_file.write(json.dumps(score_list))
 #         break
 #     elif guess > secret:
 #         print("Your guess is not correct... try something smaller")
 #     elif guess < secret:
 #         print("Your guess is not correct... try something bigger")
 
+
 #dictionary
-profilis = {
-    'vardas' : 'Benas',
-    'pavarde' : 'Jak'
-}
 
-profilis['nuotaika'] = 'gera'
+import datetime
+import json
+import random
 
-print(profilis)
+secret = random.randint(1, 30)
+attempts = 0
 
+with open("score_list.txt", "r") as score_file:
+    score_list = json.loads(score_file.read())
+    print("Top scores: " + str(score_list))
+
+for score_dict in score_list:
+    print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
+
+    if guess == secret:
+        print("You've guessed it - congratulations! It's number " + str(secret))
+        print("Attempts needed: " + str(attempts))
+
+        score_list.append({'attempts': attempts, "date": str(datetime.datetime.now())})
+
+        with open("score_list.txt", "w") as score_file:
+            score_file.write(json.dumps(score_list))
+        break
+    elif guess > secret:
+        print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
